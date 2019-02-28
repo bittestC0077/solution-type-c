d3a80a2729669f184f7acfb4252fd7719fc1ca43
bit <bit@DESKTOP-QB14LOP>
2019-02-28 11:02:38 +0900

    File 저장기능 추가

--- src/main/java/problem04/GameManager.java
+++ src/main/java/problem04/GameManager.java
@@ -41,18 +41,19 @@ public class GameManager {
 	}
 	
 	void showResult() {
-		File file = new File("/result.txt");
+		File file = new File("result.txt");
 		
 		for (int i = 0; i < userNum; i++) {
 			int score = gameList.get(i).getScore();
 			long timeDuration = gameList.get(i).getTimeDuration();	
-//			try {
-//				FileWriter fw = new FileWriter(file);
-//				fw.write(score + " / " + (18 - score) + " 경과시간: " + timeDuration + "초 \n");
-//				fw.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
+			try {
+				FileWriter fw = new FileWriter(file);
+				fw.write(score + " / " + (18 - score) + " 경과시간: " + timeDuration + "초 \n");
+				fw.flush();
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			System.out.println(score + " / " + (18 - score) + " 경과시간: " + timeDuration + "초");
 		}
 	}
